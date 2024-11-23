a8bacaf9f1934df9cd26755633ed0d13fba6d94c
SaharJaved <saharjaved95@gmail.com>
2024-11-24 02:37:13 +0500

    inspection form added

--- Routes/inspectionController.js
+++ Routes/inspectionController.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post("/addNewInspection", async (req, res) => {
     try {
-        let {customerName, mileage, yearMakeModel, contact, ro, vin, license, email, serviceDate, servicedAt, nextService,
-            interiorExteriorFields, batteryFields, underHoodFields, underVehicleFields, brakeFields, preInspectionReport, postInspectionReport} = req.body;
+        let { customerName, mileage, yearMakeModel, contact, ro, vin, license, email, serviceDate, servicedAt, nextService,
+            interiorExteriorFields, batteryFields, underHoodFields, underVehicleFields, brakeFields, preInspectionReport, postInspectionReport } = req.body;
         const inspection = new Inspection({
             customerName, mileage, yearMakeModel, contact, ro, vin, license, email, serviceDate, servicedAt, nextService,
             interiorExteriorFields, batteryFields, underHoodFields, underVehicleFields, brakeFields, preInspectionReport, postInspectionReport
@@ -21,7 +21,25 @@ router.post("/addNewInspection", async (req, res) => {
 router.get("/getRecentInspection", async (req, res) => {
     try {
         const inspection = await Inspection.findOne();
-        return res.status(200).json({ss: inspection})
+        return res.status(200).json({ ss: inspection })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ type: "Failure", message: "Internal Server Error" });
+    }
+})
+router.get("/getInspections", async (req, res) => {
+    try {
+        const inspection = await Inspection.find();
+        return res.status(200).json(inspection)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ type: "Failure", message: "Internal Server Error" });
+    }
+})
+router.get("/getInspectionById/:id", async (req, res) => {
+    try {
+        const inspection = await Inspection.findById(req.params.id);
+        return res.status(200).json({ ss: inspection })
     } catch (error) {
         console.log(error);
         return res.status(500).json({ type: "Failure", message: "Internal Server Error" });
